4caea75fa307b7cee297b897c4a1b470de7a18c8
rahul2003yadav <rahul2003ryadav@gmail.com>
2025-08-08 16:21:24 +0530

    route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,20 @@
+// import { serve } from "inngest/next";
+// import { inngest } from "@/lib/inngest/client";
+// import { generateIndustryInsights } from "@/lib/inngest/functions";
+
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest, 
+//   functions: [
+//     /* your functions will be passed here later! */
+//     [generateIndustryInsights],
+//   ],
+// });
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/functions";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
-  client: inngest, 
-  functions: [
-    /* your functions will be passed here later! */
-    [generateIndustryInsights],
-  ],
+  client: inngest,
+  functions: [generateIndustryInsights], // ← removed extra []
 });
