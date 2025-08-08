316a9c8eca2f5b55d90e2f75db5eea89289dba78
rahul2003yadav <rahul2003ryadav@gmail.com>
2025-08-08 13:31:03 +0530

    initial files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "randomuser.me"
+            }
+        ]
+    },
+    experimental: {
+        turbopack: false,
+    },
+};
 
 export default nextConfig;
